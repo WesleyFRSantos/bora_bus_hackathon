f5a51ecfa55f755f1a689901384c576995aa3d41
WESLEY FELIPE RIBEIRO DOS SANTOS <wesley.ribeiro@vocedm.com.br>
2022-09-17 13:30:17 -0300

    primeiro commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Hackathon para treinar utilização de Transformers do SKLearn',
+    author='wesley-ribeiro',
+    license='MIT',
+)
